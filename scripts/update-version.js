046111fd0d6e4b72ee51dce34d4d126d6001a02f
王雷 <raysaii1991@gmail.com>
2019-07-16 16:13:50 +0800

    (updated) update scripts

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -7,7 +7,7 @@ const run = async () => {
   const packageJSON = await readJson(packageJSONPath)
   let version = packageJSON.version
   const name = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
-  // if (name == 'dev') {
+  if (name == 'dev') {
     const tep = version.split('.').map(num => parseInt(num))
     const addVersion = (arr, str = '') => {
       if (arr.length == 1) {
@@ -25,11 +25,10 @@ const run = async () => {
     const newVersion = addVersion(tep)
     packageJSON.version = newVersion
     await writeJson(packageJSONPath, packageJSON, { spaces: 2 })
-    // execSync('git add ./package.json && git commit -m "version update" && git push', { cwd: process.cwd() })
-    // execSync('git checkout master && git merge dev', { cwd: process.cwd() })
-    execSync(`git add ./package.json && git commit -m "version update" && git tag v${newVersion} && git push origin v${newVersion} && git push`, { cwd: process.cwd() })
-    // execSync(`git checkout dev`, { cwd: process.cwd() })
-  // }
+    execSync('git add ./package.json && git commit -m "version update" && git push', { cwd: process.cwd() })
+    execSync('git checkout master && git merge dev', { cwd: process.cwd() })
+    execSync(`git tag v${newVersion} && git push origin v${newVersion} && git push`, { cwd: process.cwd() })
+  }
 }
 
 run()
